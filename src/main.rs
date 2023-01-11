da7ff0af548696c945ab0c1976d226b784ef61c4
Jamey Bryce <jybryce4@gmail.com>
2023-01-11 11:25:16 -0600

    gcd calculator web server. wow. good job such a useful app.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,68 @@
-use actix_web::{
-    web, 
-    App, 
-    HttpResponse, 
-    HttpServer
-};
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParams {
+    n: u64,
+    m: u64,
+}
 
 fn main() {
-    let server = HttpServer::new(||{
-        App::new().route("/", web::get().to(get_index))
+    let server = HttpServer::new(|| { 
+        App::new().route("/", web::get().to(get_index)) 
+                  .route("/gcd", web::post().to(post_gcd))
     });
 
     println!("Server running on http://localhost:3000");
-    server.bind("127.0.0.1:3000")
-          .expect("Error binding server to address!")
-          .run().expect("Error running server!");
-
+    server
+        .bind("127.0.0.1:3000")
+        .expect("Error binding server to address!")
+        .run()
+        .expect("Error running server!");
 }
 
 fn get_index() -> HttpResponse {
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body(
-          r#"
+    HttpResponse::Ok().content_type("text/html").body(
+        r#"
               <title>GCD Calculator</title>
               <form action="/gcd" method="post">
               <input type="text" name="n">
               <input type="text" name="m">
               <button type="submit">Compute GCD</button>
               </form>
-          "#
-        )
+          "#,
+    )
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+fn post_gcd(form: web::Form<GcdParams>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.");
+    }
+
+    let response = format!(
+        "The greatest common divisor of the numbers {} and {} \
+        is <strong>{}</strong>\n
+        ",
+        form.n,
+        form.m,
+        gcd(form.n, form.m)
+    );
+
+    HttpResponse::Ok().content_type("text/html").body(response)
 }
